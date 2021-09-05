085ce524354988b35808c31f8a8fc4ca00292467
AhmedAdelElbehairy <86647849+AhmedAdelElbehairy@users.noreply.github.com>
2021-09-05 14:54:12 +0200

    Creating Project of MCU to MCU bluetooth communication

---
+++ MCU_To_MCU_Bluetooth_Communication/MCU_To_MCU_Bluetooth_Communication/main.c
@@ -0,0 +1,18 @@
+/*
+ * MCU_To_MCU_Bluetooth_Communication.c
+ *
+ * Created: 9/5/2021 2:53:30 PM
+ * Author : vetmo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
